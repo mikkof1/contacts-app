bd34b2592fc76ee4d331efba53adac4906bc5a44
mikkof1 <mikko.laitinen@student.saimia.fi>
2017-04-11 18:27:56 +0300

    Update Angular/cli

--- client/src/app/contact/Dialogs/map-dialog/map-dialog.component.ts
+++ client/src/app/contact/Dialogs/map-dialog/map-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Contact} from "../../contact";
-import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
+//import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-map-dialog',
@@ -10,15 +10,15 @@ import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 export class MapDialogComponent implements OnInit {
 
   @Input() contact: Contact;
-  mapUrl: SafeResourceUrl;
+ // mapUrl: SafeResourceUrl;
 
-  constructor(public sanitizer: DomSanitizer) {
+  constructor(){//public sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
     let url ='www.google.fi';// 'https://www.google.fi/maps/place/' + this.contact.address + ',+' + this.contact.city;
     console.log('mapUrl: ' + url);
-    this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  //  this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
 
